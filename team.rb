d8a54bddbd38b3105d457640834e7405438981b4
takatoshi.tenkou <daiten.business@gmail.com>
2021-07-16 08:53:15 +0000

    change value

--- team.rb
+++ team.rb
@@ -3,11 +3,11 @@ class Team
     # 変数指定
     attr_accessor :name, :win, :lose, :draw
     # インスタンス初期化
-    def initialize(teamName, countWin, countLose, countDraw)
-      self.name = teamName
-      self.win = countWin
-      self.lose = countLose
-      self.draw = countDraw
+    def initialize(team_name, count_win, count_lose, count_draw)
+      self.name = team_name
+      self.win = count_win
+      self.lose = count_lose
+      self.draw = count_draw
     end
     
     # 勝率メソッド作成
@@ -16,23 +16,23 @@ class Team
     end
     
     # 成績メソッド作成
-    def show_team_result(teamName)
+    def show_team_result
         puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{self.calc_win_rate}です。"
     end
 end
 
 # インスタンス生成
-Team_G = Team.new('Giants', 67, 45, 8)
-Team_T = Team.new('Tigers', 60, 53, 7)
-Team_D = Team.new('Dragons', 60, 55, 5)
-Team_B = Team.new('BayStars', 56, 58, 6)
-Team_C = Team.new('Carp', 52, 56, 12)
-Team_S = Team.new('Swallos', 41, 69, 10)
+team_G = Team.new('Giants', 67, 45, 8)
+team_T = Team.new('Tigers', 60, 53, 7)
+team_D = Team.new('Dragons', 60, 55, 5)
+team_B = Team.new('BayStars', 56, 58, 6)
+team_C = Team.new('Carp', 52, 56, 12)
+team_S = Team.new('Swallos', 41, 69, 10)
 
 # 勝敗情報の表示
-Team_G.show_team_result('')
-Team_T.show_team_result('')
-Team_D.show_team_result('')
-Team_B.show_team_result('')
-Team_C.show_team_result('')
-Team_S.show_team_result('')
+team_G.show_team_result
+team_T.show_team_result
+team_D.show_team_result
+team_B.show_team_result
+team_C.show_team_result
+team_S.show_team_result
