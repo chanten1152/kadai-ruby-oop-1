7d5c354cb0cc421ab8c87d3700e0eff8c6be530d
takatoshi.tenkou <daiten.business@gmail.com>
2021-07-16 08:56:23 +0000

    change value second

--- team.rb
+++ team.rb
@@ -22,17 +22,17 @@ class Team
 end
 
 # インスタンス生成
-team_G = Team.new('Giants', 67, 45, 8)
-team_T = Team.new('Tigers', 60, 53, 7)
-team_D = Team.new('Dragons', 60, 55, 5)
-team_B = Team.new('BayStars', 56, 58, 6)
-team_C = Team.new('Carp', 52, 56, 12)
-team_S = Team.new('Swallos', 41, 69, 10)
+team_g = Team.new('Giants', 67, 45, 8)
+team_t = Team.new('Tigers', 60, 53, 7)
+team_d = Team.new('Dragons', 60, 55, 5)
+team_b = Team.new('BayStars', 56, 58, 6)
+team_c = Team.new('Carp', 52, 56, 12)
+team_s = Team.new('Swallos', 41, 69, 10)
 
 # 勝敗情報の表示
-team_G.show_team_result
-team_T.show_team_result
-team_D.show_team_result
-team_B.show_team_result
-team_C.show_team_result
-team_S.show_team_result
+team_g.show_team_result
+team_t.show_team_result
+team_d.show_team_result
+team_b.show_team_result
+team_c.show_team_result
+team_s.show_team_result
